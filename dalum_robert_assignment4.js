ed424223f3b2af50ff93b1b1ef13b8e1f2885ad7
robertdalum <robertdalum@yahoo.com>
2013-08-29 13:25:52 -0500

    Just addes three checks

--- dalum_robert_assignment4.js
+++ dalum_robert_assignment4.js
@@ -1,1 +1,25 @@
 alert("JavaScript works!");
+
+//Robert Dalum
+//Project 4 August 29, 2013
+// SDI 
+
+//1. Does a string follow a 123-456-7890 pattern like a phone number?
+var stringTest = function () {
+	var phoneNum = function (strng) {
+		var check = /\d{3}-\d{3}-\d{4}/;
+		return check.test(strng);
+	};
+
+//2. Does a string follow an aaa@bbb.ccc pattern like an email address?
+var emailAddress = function (strng) {
+	var check = /^\w+@[\w.\-]+\.[A-Za-z]{2,3}$/;
+	return check.test(strng);
+	};
+	
+//3. Is the string a URL (Does it start with http:// or https://)?
+var urlCheck = function (strng) {
+	var check = /^(?:http|https):/;
+	return check.test(strng);
+	};
+}
